84b1e5b77792e2de0f5b59baf1a0c2c08e26ef03
dhaval97micro1 <dhaval@micro1.ai>
2023-08-05 01:56:01 +0530

    updated meta data

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import "../globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      {" "}
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.jpeg" />
+        <title>Dhaval Shah - Portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
